35c1343ed9066f65320a7bf589bfe2230cc179a8
Torsten Schlieder <torsten.schlieder@unite.eu>
2024-01-04 16:24:19 +0100

    Error handling in REST layer

--- src/rest/handlers.rs
+++ src/rest/handlers.rs
@@ -1,4 +1,4 @@
-use axum::{Extension, Json};
+use axum::{BoxError, Extension, Json};
 use axum::extract::State;
 use axum::http::StatusCode;
 use axum::response::{IntoResponse, Response};
@@ -8,12 +8,17 @@ use crate::{Bearer, MutexSharedState};
 use crate::domain::activity::ActivityVec;
 use crate::util::iso8601;
 
-fn log_error(error: reqwest::Error) -> StatusCode {
+fn reqwest_error(error: reqwest::Error) -> StatusCode {
     warn!("{}", error);
     // Need to map reqwest::StatusCode to axum::http::StatusCode.
     // Note that both types are actually aliases of http::StatusCode, but Rust complains.
     let status = error.status().map(|e| e.as_u16()).unwrap_or(500 as u16);
     StatusCode::from_u16(status).unwrap_or(StatusCode::INTERNAL_SERVER_ERROR)
+}
+
+fn service_error(error: BoxError) -> StatusCode {
+    warn!("{}", error);
+    StatusCode::INTERNAL_SERVER_ERROR
 }
 
 #[debug_handler]
@@ -23,7 +28,7 @@ pub async fn retrieve(State(state): State<MutexSharedState>, Extension(bearer): 
     debug!("--b--> {}", &bearer.as_str()[..std::cmp::min(100, bearer.as_str().len())]);
 
     let mut guard = state.lock().await;
-    let min_time1 = (*guard).service.get_min_start_time().map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+    let min_time1 = (*guard).service.get_min_start_time().map_err(service_error)?;
     drop(guard);
 
     let mut client = reqwest::Client::new()
@@ -36,15 +41,15 @@ pub async fn retrieve(State(state): State<MutexSharedState>, Extension(bearer): 
     }
 
     let activities : ActivityVec = client
-        .send().await.map_err(log_error)?
-        .error_for_status().map_err(log_error)?
-        .json::<ActivityVec>().await.map_err(log_error)?;
+        .send().await.map_err(reqwest_error)?
+        .error_for_status().map_err(reqwest_error)?
+        .json::<ActivityVec>().await.map_err(reqwest_error)?;
 
     //info!("--r--> {:?}", result);
 
     let mut guard = state.lock().await;
-    let min_time2 = (*guard).service.add(&activities).map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-    // Be safe although min_time2 <= min_time1 should always hold:
+    let min_time2 = (*guard).service.add(&activities).map_err(service_error)?;
+    // Calculate minimum although min_time2 <= min_time1 should always hold:
     (*guard).min_activity_time = iso8601::min_secs(min_time1, min_time2);
 
     Ok(Json(activities).into_response()) // TODO: Do someting with the result
