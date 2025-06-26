1936c9b3c0cec47a45b7c62775c51c4dcc9e76d3
Torsten Schlieder <torsten.schlieder@unite.eu>
2025-06-26 16:36:53 +0200

    tile_creator skeleton and run logic

---
+++ src/bin/tile_creator.rs
@@ -0,0 +1,7 @@
+fn main() {
+    println!("Hello");
+    // Iterate over activities by increasing start_date
+    //   If an activity has gpx_fetched = 1 then
+    //     load the corresponding GPX file (TODO: This is not possible at the moment, see https://docs.rs/gpx/latest/gpx/)
+    //     generate and write the tiles for the corresponding activityID
+}
