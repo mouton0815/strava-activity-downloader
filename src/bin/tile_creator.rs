98adbec7f080854a78bbc1afdfd9516c4debc591
Torsten Schlieder <torsten.schlieder@unite.eu>
2025-06-28 19:09:52 +0200

    Use library GPX writing

--- src/bin/tile_creator.rs
+++ src/bin/tile_creator.rs
@@ -1,7 +1,33 @@
+use std::fs::File;
+use std::io::BufReader;
+use gpx::{Gpx, read, Track, TrackSegment};
+
+// TODO: Use crate geo_types!!
+
 fn main() {
     println!("Hello");
     // Iterate over activities by increasing start_date
     //   If an activity has gpx_fetched = 1 then
     //     load the corresponding GPX file (TODO: This is not possible at the moment, see https://docs.rs/gpx/latest/gpx/)
     //     generate and write the tiles for the corresponding activityID
+
+    let file = File::open("/Users/torsten/git/strava-activity-downloader/data/2024/07/11936054836.gpx").unwrap();
+    let reader = BufReader::new(file);
+
+    // read takes any io::Read and gives a Result<Gpx, Error>.
+    let gpx: Gpx = read(reader).unwrap();
+
+    // Each GPX file has multiple "tracks", this takes the first one.
+    let track: &Track = &gpx.tracks[0];
+    assert_eq!(track.name, Some(String::from("Von der Bahn zum Hotel")));
+
+    // Each track will have different segments full of waypoints, where a
+    // waypoint contains info like latitude, longitude, and elevation.
+    let segment: &TrackSegment = &track.segments[0];
+
+    // This is an example of retrieving the elevation (in meters) at certain points.
+    for point in &segment.points {
+        println!("{:?}", point.point().x());
+    }
+    println!("{:?}", segment.points[0].elevation);
 }
--- src/domain/activity_stream.rs
+++ src/domain/activity_stream.rs
@@ -1,13 +1,13 @@
 use std::collections::BTreeSet;
 use std::fmt;
-use std::fmt::Write;
-use std::io::Read;
+use std::io::{Read, Write};
 use axum::BoxError;
 use serde::Deserialize;
-use gpx::{Gpx, read, Track, TrackSegment};
+use geo_types::Point;
+use gpx::{Gpx, GpxVersion, Link, Metadata, read, Track, TrackSegment, Waypoint};
 use iso8601_timestamp::time::OffsetDateTime;
 use crate::domain::map_tile::MapTile;
-use crate::util::iso8601::{secs_to_string, string_to_secs};
+use crate::util::iso8601::string_to_secs;
 
 type LatLon = (f64, f64); // TODO: Use crate geo_types!!
 
@@ -75,44 +75,68 @@ impl ActivityStream {
         Ok(stream)
     }
 
-    pub fn to_gpx(&self, activity_id: u64, activity_name: &str, start_time: &str) -> Result<String, BoxError> {
+    pub fn to_gpx<W: Write>(&self, writer: W, activity_id: u64, activity_name: &str, start_time: &str) -> Result<(), BoxError> {
         if self.latlng.data.len() != self.time.data.len() ||
             self.time.data.len() != self.altitude.data.len() {
             Err("Streams have different lengths".into())
         } else {
             let start_time = string_to_secs(start_time);
-            self.to_gpx_internal(activity_id, activity_name, start_time).map_err(|_| "Formatting error".into())
+            self.to_gpx_internal(writer, activity_id, activity_name, start_time).map_err(|_| "Formatting error".into())
         }
     }
 
-    fn to_gpx_internal(&self, activity_id: u64, activity_name: &str, start_time: i64) -> Result<String, fmt::Error> {
+    fn to_gpx_internal<W: Write>(&self, writer: W, activity_id: u64, activity_name: &str, start_time: i64) -> Result<(), BoxError> {
         // Escape name according to https://stackoverflow.com/questions/21758345/what-are-the-official-xml-reserved-characters
         let name = activity_name.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;");
-        let mut s = String::new();
-        writeln!(&mut s, "<?xml version='1.0' encoding='UTF-8'?>")?;
-        writeln!(&mut s, "<gpx xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns='http://www.topografix.com/GPX/1/1' xsi:schemaLocation='http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd' version='1.1' creator='http://strava.com/'>")?;
-        writeln!(&mut s, "  <metadata>")?;
-        writeln!(&mut s, "    <name>{}</name>", name)?;
-        writeln!(&mut s, "    <link href='https://www.strava.com/api/v3/activities/{}'>", activity_id)?;
-        writeln!(&mut s, "      <text>{}</text>", name)?;
-        writeln!(&mut s, "    </link>")?;
-        writeln!(&mut s, "  </metadata>")?;
-        writeln!(&mut s, "  <trk>")?;
-        writeln!(&mut s, "    <name>{}</name>", name)?;
-        writeln!(&mut s, "    <trkseg>")?;
+
+        let mut points: Vec<Waypoint> = Vec::new();
         for i in 0..self.latlng.data.len() {
-            let (lat, lon) = &self.latlng.data[i.clone()];
-            let altitude = &self.altitude.data[i.clone()];
-            let time = self.time.data[i].clone() as i64;
-            writeln!(&mut s, "      <trkpt lat='{}' lon='{}'>", lat, lon)?;
-            writeln!(&mut s, "        <ele>{:?}</ele>", altitude)?;
-            writeln!(&mut s, "        <time>{}</time>", secs_to_string(start_time + time))?;
-            writeln!(&mut s, "      </trkpt>")?;
+            let (lat, lon) = &self.latlng.data[i];
+            let altitude = &self.altitude.data[i];
+            let time = start_time + self.time.data[i] as i64;
+            let time= OffsetDateTime::from_unix_timestamp(time).unwrap();
+            let mut point = Waypoint::new(Point::new(*lon, *lat)); // TODO: Get point directly once change to geo_types is done
+            point.elevation = Some(*altitude);
+            point.time = Some(time.into());
+            points.push(point);
         }
-        writeln!(&mut s, "    </trkseg>")?;
-        writeln!(&mut s, "  </trk>")?;
-        writeln!(&mut s, "</gpx>")?;
-        Ok(s)
+        let track_segment = TrackSegment {
+            points
+        };
+        let track = Track {
+            name: Some(name.clone()),
+            comment: None,
+            description: None,
+            source: None,
+            links: vec![],
+            type_: None,
+            number: None,
+            segments: vec![track_segment]
+        };
+        let metadata = Metadata {
+            name: Some(name.clone()),
+            description: None,
+            author: None,
+            links: vec![Link {
+                href: format!("https://www.strava.com/api/v3/activities/{}", activity_id),
+                text: Some(name),
+                type_ : None
+            }],
+            time: None,
+            keywords: None,
+            copyright: None,
+            bounds: None,
+        };
+        let gpx = Gpx {
+            version: GpxVersion::Gpx11,
+            creator: Some("http://strava.com/".to_string()),
+            metadata: Some(metadata),
+            waypoints: vec![],
+            tracks: vec![track],
+            routes: vec![],
+        };
+        gpx::write(&gpx, writer)?;
+        Ok(())
     }
 
     /// Returns the list of unique [MapTile]s touched by this activity stream.
@@ -142,46 +166,47 @@ mod tests {
     use crate::domain::map_tile::MapTile;
 
     // Activity streams from java have additional fields like "series_type". They are ignored here.
-    static INPUT: &str = r#"{
+    static STREAM: &str = r#"{
   "latlng":{"data":[[51.318165,12.375655],[51.318213,12.395588],[51.318213,12.375588]]},
   "altitude":{"data":[123.456,120.0,100.0]},
   "distance":{"data":[0,1.3,3.7]},
   "time":{"data":[0,3,7]}
 }"#;
 
-    static GPX_REF: &str = r#"<?xml version='1.0' encoding='UTF-8'?>
-<gpx xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns='http://www.topografix.com/GPX/1/1' xsi:schemaLocation='http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd' version='1.1' creator='http://strava.com/'>
+    static GPX_REF: &str = r#"<?xml version="1.0" encoding="UTF-8"?>
+<gpx version="1.1" xmlns="http://www.topografix.com/GPX/1/1" creator="http://strava.com/">
   <metadata>
     <name>Foo Bar</name>
-    <link href='https://www.strava.com/api/v3/activities/12345'>
+    <link href="https://www.strava.com/api/v3/activities/12345">
       <text>Foo Bar</text>
     </link>
   </metadata>
   <trk>
     <name>Foo Bar</name>
     <trkseg>
-      <trkpt lat='51.318165' lon='12.375655'>
+      <trkpt lat="51.318165" lon="12.375655">
         <ele>123.456</ele>
-        <time>2024-01-01T00:00:00Z</time>
+        <time>2024-01-01T00:00:00.000000000Z</time>
       </trkpt>
-      <trkpt lat='51.318213' lon='12.395588'>
-        <ele>120.0</ele>
-        <time>2024-01-01T00:00:03Z</time>
+      <trkpt lat="51.318213" lon="12.395588">
+        <ele>120</ele>
+        <time>2024-01-01T00:00:03.000000000Z</time>
       </trkpt>
-      <trkpt lat='51.318213' lon='12.375588'>
-        <ele>100.0</ele>
-        <time>2024-01-01T00:00:07Z</time>
+      <trkpt lat="51.318213" lon="12.375588">
+        <ele>100</ele>
+        <time>2024-01-01T00:00:07.000000000Z</time>
       </trkpt>
     </trkseg>
   </trk>
-</gpx>
-"#;
+</gpx>"#;
 
     #[test]
     fn test_to_gpx() {
-        let stream : serde_json::Result<ActivityStream> = serde_json::from_str(INPUT);
+        let stream : serde_json::Result<ActivityStream> = serde_json::from_str(STREAM);
         assert!(stream.is_ok());
-        let result = stream.unwrap().to_gpx(12345, "Foo Bar", "2024-01-01T00:00:00Z");
+        let mut buffer: Vec<u8> = Vec::new();
+        assert!(stream.unwrap().to_gpx(&mut buffer, 12345, "Foo Bar", "2024-01-01T00:00:00Z").is_ok());
+        let result = String::from_utf8(buffer);
         assert!(result.is_ok());
         assert_eq!(result.unwrap(), GPX_REF);
     }
@@ -202,7 +227,7 @@ mod tests {
 
     #[test]
     fn test_to_tiles() {
-        let stream : serde_json::Result<ActivityStream> = serde_json::from_str(INPUT);
+        let stream : serde_json::Result<ActivityStream> = serde_json::from_str(STREAM);
         assert!(stream.is_ok());
         let result = stream.unwrap().to_tiles(14);
         assert!(result.is_ok());
