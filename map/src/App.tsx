fbbe06d766ed0ae013c7fce87122da2fdc002f55
Torsten Schlieder <torsten.schlieder@unite.eu>
2025-08-12 15:13:22 +0200

    Fetch only displayable tiles (no caching yet)

--- map/src/App.tsx
+++ map/src/App.tsx
@@ -1,32 +1,55 @@
-import { Suspense, use } from 'react'
-import { divIcon, marker } from 'leaflet'
-import { MapContainer, Rectangle, TileLayer, useMap } from 'react-leaflet'
-import { Coords, Tile } from 'tiles-math'
+import { Suspense, use, useEffect, useState } from 'react'
+import { divIcon, LatLng, LatLngBounds, LatLngTuple, marker } from 'leaflet'
+import { MapContainer, Rectangle, TileLayer, useMapEvents } from 'react-leaflet'
+import { Coords, coords2tile, Tile, TileNo } from 'tiles-math'
 import './App.css'
 
-const SERVER_URL = '' // Base URL of the Rust server, use http://localhost:2525 in dev mode
+const SERVER_URL = 'http://localhost:2525' // Base URL of the Rust server, use http://localhost:2525 in dev mode
 const TILES_URL = `${SERVER_URL}/tiles`
 
 const TILE_ZOOM = 14
 const CROSSHAIR_SIZE = 50
+const DEFAULT_CENTER: LatLngTuple = [51.33962, 12.37129] // Leipzig (will be relocated if user gives consent)
+
+class TileBounds {
+    x1: number
+    y1: number
+    x2: number
+    y2: number
+    constructor(upperLeft: TileNo, lowerRight: TileNo) {
+        this.x1 = upperLeft.x
+        this.y1 = upperLeft.y
+        this.x2 = lowerRight.x
+        this.y2 = lowerRight.y
+    }
+}
 
 // TODO: Clip to map screen and reload if needed?
-async function loadTiles(): Promise<Array<Coords>> {
+async function loadTiles(bounds: TileBounds | null): Promise<Array<Coords>> {
     // return Promise.resolve([[8755,5460],[8755,5461]])
-    try {
-        const response = await fetch(`${TILES_URL}/${TILE_ZOOM}`)
-        return await response.json()
-    } catch (e) {
-        console.warn('Cannot fetch data from server:', e)
-        return []
+    if (bounds) {
+        const boundsParam = `bounds=${bounds.x1},${bounds.y1},${bounds.x2},${bounds.y2}`
+        try {
+            const response = await fetch(`${TILES_URL}/${TILE_ZOOM}?${boundsParam}`)
+            return await response.json()
+        } catch (e) {
+            console.warn('Cannot fetch data from server:', e)
+        }
     }
+    return []
+}
+
+function getTileBounds(bounds: LatLngBounds): TileBounds {
+    return new TileBounds(
+        coords2tile([bounds.getNorth(), bounds.getWest()], TILE_ZOOM),
+        coords2tile([bounds.getSouth(), bounds.getEast()], TILE_ZOOM))
 }
 
 export function App() {
     return (
         <MapContainer
             zoomSnap={0.1}
-            center={[51.33962, 12.37129]}
+            center={DEFAULT_CENTER}
             zoom={11}
             scrollWheelZoom={true}
             style={{ height: '100vh', minWidth: '100vw' }}>
@@ -34,10 +57,60 @@ export function App() {
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Suspense fallback={<div>Loading...</div>}>
-                <TileContainer tilesPromise={loadTiles()} />
-            </Suspense>
+            <LoadContainer />
         </MapContainer>
+    )
+}
+
+function LoadContainer() {
+    const [location, setLocation] = useState<LatLng | null>(null)
+    const [bounds, setBounds] = useState<TileBounds | null>(null)
+
+    const map = useMapEvents({
+        click: () => {
+            // console.log('-----> locate ...')
+            map.locate({setView: true, maxZoom: TILE_ZOOM})
+        },
+        locationfound: (event) => {
+            // console.log('-----> location found:', event)
+            const icon = divIcon({
+                className: 'crosshair-marker',
+                iconSize: [CROSSHAIR_SIZE, CROSSHAIR_SIZE],
+                iconAnchor: [CROSSHAIR_SIZE / 2, CROSSHAIR_SIZE / 2] // Centered
+            })
+            marker(event.latlng, { icon }).addTo(map);
+        },
+        locationerror: (event) => {
+            console.warn(event.message)
+            alert(event.message)
+        },
+        moveend: () => {
+            // console.log('-----> moved')
+            setBounds(getTileBounds(map.getBounds()))
+        },
+        zoomend: () => {
+            // console.log('-----> zoomed')
+            setBounds(getTileBounds(map.getBounds()))
+        },
+        viewreset: () => {
+            // console.log('-----> reset')
+            setBounds(getTileBounds(map.getBounds()))
+        }
+    })
+
+    useEffect(() => {
+        if (location === null) {
+            // console.log('-----> locate ...')
+            map.locate({setView: true, maxZoom: TILE_ZOOM})
+            setLocation(map.getCenter())
+        }
+    }, [location])
+
+    // console.log("-----> PASS with ", bounds)
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <TileContainer tilesPromise={loadTiles(bounds)} />
+        </Suspense>
     )
 }
 
@@ -47,23 +120,6 @@ type TileContainerProps = {
 
 function TileContainer({ tilesPromise }: TileContainerProps) {
     const tiles = use(tilesPromise)
-    //console.log('----->', data)
-    const map = useMap()
-    map.on('locationfound', (e) => {
-        const icon = divIcon({
-            className: 'crosshair-marker',
-            iconSize: [CROSSHAIR_SIZE, CROSSHAIR_SIZE],
-            iconAnchor: [CROSSHAIR_SIZE / 2, CROSSHAIR_SIZE / 2] // Centered
-        })
-        marker(e.latlng, { icon }).addTo(map);
-        //circle(e.latlng, { radius: 50 }).addTo(map)
-    })
-    map.on('locationerror', (e) => {
-        console.warn(e.message)
-        alert(e.message)
-    })
-    map.locate({setView: true, maxZoom: TILE_ZOOM})
-
     return (
         <div>
             {tiles.map(coords => Tile.of(coords[0], coords[1], TILE_ZOOM)).map((tile, index) =>
