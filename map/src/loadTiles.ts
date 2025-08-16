99594db89750c4ccf6edb545aee81b04ea459679
Torsten Schlieder <torsten.schlieder@unite.eu>
2025-08-16 18:06:06 +0200

    Final clipping

--- map/src/loadTiles.ts
+++ map/src/loadTiles.ts
@@ -3,7 +3,7 @@ import { TileArray } from './TileCache.ts'
 
 export async function loadTiles(tilesUrl: string, bounds: TileBounds, zoom: number): Promise<TileArray> {
     try {
-        const boundsParam = `bounds=${bounds.x1 + 2},${bounds.y1 + 2},${bounds.x2 - 2},${bounds.y2 - 2}`
+        const boundsParam = `bounds=${bounds.x1},${bounds.y1},${bounds.x2},${bounds.y2}`
         const response = await fetch(`${tilesUrl}/${zoom}?${boundsParam}`)
         return await response.json()
     } catch (e) {
