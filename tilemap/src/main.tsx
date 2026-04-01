0067c2e0a7897e00bb43bb771166db75012bc136
Torsten Schlieder <torsten.schlieder@unite.eu>
2026-04-01 13:45:38 +0200

    Removed tilemap frontend

--- tilemap/src/main.tsx
+++
@@ -1,7 +0,0 @@
-import ReactDOM from 'react-dom/client'
-import { App } from './App.tsx'
-import 'leaflet/dist/leaflet.css'
-
-// eslint-disable-next-line no-undef
-const root = ReactDOM.createRoot(document.getElementById('root')!)
-root.render(<App />)
